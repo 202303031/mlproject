06ded1a63c4b00ee28674f586d354f105205a5fd
202303031 <dhruvramani5566@gmail.com>
2026-06-18 15:21:49 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,13 +1,17 @@
 import os
-import sys  # Added missing sys import
-import src
-from src.components.data_transformation import DataTransformation
+import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -18,35 +22,34 @@ class DataIngestionConfig:
 
 
 class DataIngestion:
-    # Setting a default value makes 'config' optional during initialization
-    def __init__(self, config: DataIngestionConfig = DataIngestionConfig()):
-        self.ingestion_config = config
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Initiating data ingestion")
+        logging.info("Entered the data ingestion method or component")
         try:
-            # CORRECTED: Added 'r' before the string to fix the invalid escape sequence bug
-            df = pd.read_csv(r"C:\WebServer\PythonProject\notebook\data\stud.csv")
-            logging.info("Data has been loaded")
+            df = pd.read_csv(r'notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
 
-            # FIXED: Added exist_ok=True so it doesn't crash if 'artifacts' already exists
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            logging.info("Train and Test split is initiated")
+            logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
-            logging.info("Data has been saved")
+            logging.info("Inmgestion of the data iss completed")
+
             return (
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path
+
             )
         except Exception as e:
-            # FIXED: Passed sys so CustomException works correctly
             raise CustomException(e, sys)
 
 
@@ -56,3 +59,8 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+
+
